c9fecd2d77bcb2c63d81e5cd4ad609ada5a6642a
ayushmankd@live.com <ayushmankd@live.com>
2020-02-11 19:26:09 +0530

    All File Done

---
+++ ClientTCPFile.java
@@ -0,0 +1,75 @@
+// Client Program to send Any File to Server through TCP
+// Progress Bar
+// Client Choose his own file
+import java.io.*;  
+import java.net.*; 
+import java.util.*; 
+public class ClientTCPFile {  
+    public static void main(String[] args) throws Exception{  
+        try{      
+            // Initialization
+            System.out.println("Client is sending Text File..");
+            Socket s=new Socket("localhost",6666);  
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            System.out.println("Select the Options From Below (Only Enter the Number): ");
+            System.out.println("1. Send a Text File");
+            System.out.println("2. Send a Image File");
+            System.out.println("3. Send a Audio File");
+            System.out.println("4. Send a Video File");
+            Scanner scan = new Scanner(System.in);
+            int option = scan.nextInt();
+            scan.close();
+            dout.writeUTF(""+option);
+            File file = null;
+            switch (option) {
+                case 1:
+                    file = new File("./ExampleMessage.txt");
+                    break;
+                case 2:
+                    file = new File("./Image.jpg");
+                    break;
+                case 3:
+                    file = new File("./sample_audio.ogg");
+                    break;
+                case 4:
+                    file = new File("./sample_video.mp4");
+                    break;
+                default:
+                    break;
+            }
+            // File Operations
+            FileInputStream fin = new FileInputStream(file);
+            long fileLength = file.length();
+            int fileLengthInt = Math.toIntExact(fileLength);
+
+            // Write the file into a byte Array and send the file length to 
+            // Server
+            byte[] byteArray = new byte[fileLengthInt];
+            fin.read(byteArray);
+            dout.writeUTF(""+fileLength);  
+            dout.flush();
+
+            int bufferSize = 5;
+            String response = (String)din.readUTF();
+            int currOffset = 0;
+            byte[] bytes = new byte[bufferSize];
+            while (response.compareTo("OK") == 0) {
+                if ((currOffset+bufferSize) <= fileLengthInt) {
+                    bytes = Arrays.copyOfRange(byteArray, currOffset, currOffset+bufferSize);
+                    dout.write(bytes);
+                } else {
+                    bytes = Arrays.copyOfRange(byteArray, currOffset, fileLengthInt);
+                    dout.write(bytes);
+                }
+                dout.flush();
+                currOffset += bufferSize;
+                response = (String)din.readUTF();
+            }
+            fin.close();
+            dout.flush();  
+            dout.close();  
+            s.close();  
+        }catch(Exception e){System.out.println(e);}  
+    }  
+}  
---
+++ ServerTCPFile.java
@@ -0,0 +1,59 @@
+// Server Program to Recieve Any File From Client through TCP
+// To Do Progress Bar
+import java.io.*;  
+import java.net.*;  
+public class ServerTCPFile {  
+    public static void main(String[] args){  
+        try{  
+            System.out.println("Server Started..");
+            ServerSocket ss=new ServerSocket(6666);  
+            Socket s=ss.accept();  
+            DataInputStream din=new DataInputStream(s.getInputStream());  
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            int fileOption = Integer.parseInt(din.readUTF());
+            long fileLengthComing = Long.parseLong(din.readUTF());
+            dout.writeUTF("OK");
+            File file = null;
+            switch (fileOption) {
+                case 1:
+                    file = new File("./ServerText.txt");
+                    break;
+                case 2:
+                    file = new File("./ServerImage.jpg");
+                    break;
+                case 3:
+                    file = new File("./ServerAudio.ogg");
+                    break;
+                case 4:
+                    file = new File("./ServerVideo.mp4");
+                    break;
+                
+                default:
+                    break;
+            }
+            FileOutputStream fout = new FileOutputStream(file);
+            int fileLengthComingInt = Math.toIntExact(fileLengthComing);
+            int bufferSize = 5;
+            byte[] byteArr = new byte[fileLengthComingInt];
+            int currentOffset = 0;
+            byte[] bytes = new byte[bufferSize];
+            while ((currentOffset+bufferSize) < fileLengthComingInt) {
+                din.read(bytes);
+                for(int i=0;i<bufferSize;i++){
+                    byteArr[currentOffset] = bytes[i];
+                    currentOffset+=1;
+                }
+                dout.writeUTF("OK");
+            }
+            din.read(bytes);
+            for(int i=0; i<fileLengthComingInt-currentOffset; i++){
+                byteArr[currentOffset+i] = bytes[i];
+            }
+            fout.write(byteArr);
+            System.out.println("DONE");
+            dout.writeUTF("Done");
+            fout.close();
+            ss.close();  
+        }catch(Exception e){System.out.println(e);}  
+    }  
+}  
